67682db8eee80557de7641da26eb7decb8de6a2a
Ozziekins <nenubariozioma@gmail.com>
2021-01-09 01:38:21 +0300

    news plus section

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
 // $('#myCarousel').carousel({
 //     interval: 9000
 //   })
-  
+
 //   $('.carousel .carousel-item').each(function(){
 //     var next = $(this).next();
 //     if (!next.length) {
 //       next = $(this).siblings(':first');
 //     }
 //     next.clone().appendTo($(this));
-    
+
 //     for (var i=0;i<2;i++) {
 //       next=next.next();
 //       if (!next.length) {
 //           next = $(this).siblings(':first');
 //         }
-      
+    
 //       next.clone().appendTo($(this));
 //     }
 //   });
 
-$('#myCarousel').on('slide.bs.carousel', function (e) {
+$('#myCarousel').carousel({
+    interval: 9000
+})
 
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
+$('.carousel .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
         }
+
+        next.children(':first-child').clone().appendTo($(this));
     }
 });
