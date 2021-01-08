f330abf7a12f1fe167afbf1b37655d3d378186dd
elamboatonge <elamboatonge@gmail.com>
2021-01-09 01:29:03 +0300

    latest updates

---
+++ Новости/js/script.js
@@ -0,0 +1,42 @@
+// $('#myCarousel').carousel({
+//     interval: 9000
+//   })
+
+//   $('.carousel .carousel-item').each(function(){
+//     var next = $(this).next();
+//     if (!next.length) {
+//       next = $(this).siblings(':first');
+//     }
+//     next.clone().appendTo($(this));
+
+//     for (var i=0;i<2;i++) {
+//       next=next.next();
+//       if (!next.length) {
+//           next = $(this).siblings(':first');
+//         }
+    
+//       next.clone().appendTo($(this));
+//     }
+//   });
+
+$('#myCarousel').carousel({
+    interval: 9000
+})
+
+$('.carousel .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
