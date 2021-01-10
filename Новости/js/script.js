58ef35c3ba9281032044bb9022da4d0527945fd2
elamboatonge <elamboatonge@gmail.com>
2021-01-10 16:05:46 +0300

    working on slider

--- Новости/js/script.js
+++ Новости/js/script.js
@@ -18,29 +18,27 @@
 //       next.clone().appendTo($(this));
 //     }
 //   });
-
-$('#myCarousel').carousel({
-    interval: 9000
-})
-
-$('.carousel .carousel-item').each(function() {
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-
-    for (var i = 0; i < minPerSlide; i++) {
-        next = next.next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-
-        next.children(':first-child').clone().appendTo($(this));
-    }
+jQuery(document).ready(function() {
+	$('#carousel-example').on('slide.bs.carousel', function (e) {
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 3;
+	    var totalItems = $('.carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+	            }
+	            else {
+	                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+	            }
+	        }
+	    }
+	});
 });
-
 
 // <!--modal-->
 // $(document).ready(function() {
