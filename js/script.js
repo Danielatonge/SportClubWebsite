1c6dff9f2ee2b690a57436874a4e3cbe693e7160
elamboatonge <elamboatonge@gmail.com>
2021-02-04 01:19:01 +0300

    latest

--- js/script.js
+++ js/script.js
@@ -50,12 +50,30 @@ $('.carousel .carousel-item').each(function() {
 //     });
 //  });
 
+// Get the navbar
+var navbar = $(".navbar")[0];
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
 $(document).ready(function() {
-    // executes when HTML-Document is loaded and DOM is ready
-   console.log("document is ready");
-     
+  // executes when HTML-Document is loaded and DOM is ready
+  console.log("document is ready");
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
    
-     $( ".card-shadow" ).hover(
+  $( ".card-shadow" ).hover(
      function() {
        $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
      }, function() {
@@ -65,3 +83,4 @@ $(document).ready(function() {
      
    // document ready  
    });
+
