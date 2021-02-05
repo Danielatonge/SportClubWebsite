1b1f2f58f9578d8ec7279045c0554f6d3cc7460a
elamboatonge <elamboatonge@gmail.com>
2021-02-05 09:20:21 +0300

    latest

--- js/script.js
+++ js/script.js
@@ -1,86 +1,33 @@
-// $('#myCarousel').carousel({
-//     interval: 9000
-//   })
-
-//   $('.carousel .carousel-item').each(function(){
-//     var next = $(this).next();
-//     if (!next.length) {
-//       next = $(this).siblings(':first');
-//     }
-//     next.clone().appendTo($(this));
-
-//     for (var i=0;i<2;i++) {
-//       next=next.next();
-//       if (!next.length) {
-//           next = $(this).siblings(':first');
-//         }
-    
-//       next.clone().appendTo($(this));
-//     }
-//   });
-
-$('#myCarousel').carousel({
-    interval: 9000
-})
-
-$('.carousel .carousel-item').each(function() {
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-
-    for (var i = 0; i < minPerSlide; i++) {
-        next = next.next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-
-        next.children(':first-child').clone().appendTo($(this));
-    }
-});
-
-
-// <!--modal-->
-// $(document).ready(function() {
-//     $("#MyModal").modal();
-//     $('#myModal').on('shown.bs.modal', function() {
-//        $('#myInput').focus();
-//     });
-//  });
-
-// Get the navbar
-var navbar = $(".navbar")[0];
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-
-
 $(document).ready(function() {
   // executes when HTML-Document is loaded and DOM is ready
   console.log("document is ready");
-  // When the user scrolls the page, execute myFunction
-  window.onscroll = function() {myFunction()};
    
   $( ".card-shadow" ).hover(
      function() {
        $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
      }, function() {
        $(this).removeClass('shadow-lg');
-     }
-   );
+     });
      
    // document ready  
-   });
+   $('#myCarousel').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    console.log("compare:",totalItems-(itemsPerSlide-1));
+    console.log(e.direction);
+    if(idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+        // append slides to end
+        if (e.direction=="left") {
+            $('.carousel-item').eq(i).appendTo('.carousel-inner');
+        } else {
+            $('.carousel-item').eq(0).appendTo('.carousel-inner');
+        }
+      }
+    }
+  });
+});
 
