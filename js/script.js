4284321aabd85162bc88f66a994a654455cd07a9
Djotchuang <djotchuangtamo@gmail.com>
2021-09-11 20:21:10 +0100

    Fixed carousel nav design and display

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-  // executes when HTML-Document is loaded and DOM is ready
-  // console.log("document is ready");
-
   let slider = tns({
     container: ".my-slider",
     items: 1,
@@ -21,7 +18,6 @@ $(document).ready(function () {
     controlsContainer: "#controls",
     prevButton: ".previous",
     nextButton: ".next",
-    autoplay: true,
     navPosition: "bottom",
     mouseDrag: true,
   });
